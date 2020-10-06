70bf160c21ab9ce7631263a83c23be6fe7258b76
Puja <pujayadav@gmail.com>
2020-10-06 20:43:19 +0530

    [puja] add . WeLcome Message

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome To The Registration Process");
+            Console.WriteLine("=============================================");
+        }
+    }
+}
