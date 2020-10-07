3c5e6fc6b3c988b08cbb29b3dccd7cab356fa2ea
Puja <pujayadav@gmail.com>
2020-10-07 16:12:26 +0530

    [puja] add. Changes in password

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -8,6 +8,16 @@ namespace UserRegistration
         {
             Console.WriteLine("WelCome To The Registration Process");
             Console.WriteLine("=============================================");
+
+            User user = new User();
+            user.verifyfirstName();
+            user.verifylastName();
+            user.verifyEmail();
+            user.phoneNo();
+            user.PassWordVerification();
+            Console.WriteLine("\nName : " + user.FirstName + " " + user.LastName);
+            Console.WriteLine("Email : " + user.emailId);
+            Console.WriteLine("Ph. Number : " + user.PhoneNumber);
         }
     }
 }
